18c80c4ad14015492b845617a02141bcc428a1c4
juninbr2000 <edson.gentil.junior@gmail.com>
2024-08-30 16:03:48 -0300

    ajustes e copy

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const navButton = document.querySelector('.nav_button');
 const nav = document.querySelector('.nav_menu')
 
+const inputValue = document.querySelector('#email')
+const buttonCopy = document.querySelector('.copy')
 
 navButton.addEventListener('click', () => {
     nav.classList.toggle('ocult')
+})
+
+buttonCopy.addEventListener('click', () => {
+    inputValue.select()
+    document.execCommand('copy')
 })
