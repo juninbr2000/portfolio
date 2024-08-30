1fab5c62805449f0a50ab7454e3b48404b95ebe2
juninbr2000 <edson.gentil.junior@gmail.com>
2024-08-30 15:03:35 -0300

    Novo design

--- index.js
+++ index.js
@@ -1,42 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const containerElement = document.querySelector(".container");
-    const darkModeButton = document.querySelector("#mode")
-    const inputvalue = document.querySelector('#email')
-    const copyBtn = document.querySelector('.copy')
+const navButton = document.querySelector('.nav_button');
+const nav = document.querySelector('.nav_menu')
 
-    const theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    
-    const updateThemeColor = () => {
-        if (theme === true && containerElement) {
-            // Adiciona a classe "dark" ao elemento .container se o tema for escuro
-            containerElement.classList.add("dark");
-            verifyIconDarkMode()
-        }
-    };
 
-    const verifyIconDarkMode = () => {
-        if(containerElement.classList.contains("dark")){
-            darkModeButton.innerHTML = '<i class="fa-solid fa-moon"></i>'
-        } else {
-            darkModeButton.innerHTML = '<i class="fa-solid fa-sun"></i>'
-        }
-    }
-
-    darkModeButton.addEventListener("click", () => {
-        containerElement.classList.toggle("dark");
-        verifyIconDarkMode()
-    })
-
-    copyBtn.addEventListener('click', () => {
-        console.log('clicou')
-        inputvalue.select()
-        copyBtn.innerHTML = '<i class="fa-solid fa-paperclip"></i> Copiado!'
-        document.execCommand('copy');
-
-        setTimeout(()=>{
-            copyBtn.innerHTML = '<i class="fa-solid fa-paperclip"></i> Copiar email'
-            }, 3000) 
-    })
-    
-    updateThemeColor();
-});
+navButton.addEventListener('click', () => {
+    nav.classList.toggle('ocult')
+})
